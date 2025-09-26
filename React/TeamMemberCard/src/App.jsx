54ec5eefae75e879845d31136d7d819bcf73d46f
scu-wtag <sophia.cuarte@welldev.io>
2025-09-26 17:17:41 +0200

    Team Cards done

---
+++ React/TeamMemberCard/src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { TeamMembers } from './TeamMemberCard'
+
+function App() {
+
+  const team = [
+    { name: "Amy", age: 30, pet: "Rabbit called Buns"},
+    { name: "Thomas", age: 49, pet: "Budgi names Tim"},
+    { name: "Joanna", age: 25, pet: "Golden Retriever called Buddy"},
+    { name: "Jake", age: 19, pet: "Axolotl named Axy"},
+
+  ]
+  return (
+    <div style={{justifyContent: "center", }}>
+      <h1>Our Team</h1>
+      <div style={{display: 'flex', gap: '7px', flexWrap: 'wrap'}}
+        >
+        {team.map((u) => (
+          <TeamMembers
+          name = {u.name}
+          age = {u.age}
+          pet = {u.pet}
+        />
+        ))}
+      </div>
+    </div>
+
+  );
+}
+
+export default App
