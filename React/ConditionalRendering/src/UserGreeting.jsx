55b186a0ec936c877c2bda24c326dd286876b4a9
scu-wtag <sophia.cuarte@welldev.io>
2025-10-07 09:33:22 +0200

    conditional rendering

---
+++ React/ConditionalRendering/src/UserGreeting.jsx
@@ -0,0 +1,9 @@
+
+function UserGreeting(props) {
+
+    const welcomeMessage = <h2 className="welcome-message">Welcome {props.username}</h2>
+    const loginPrompt = <h2 className="login-prompt"> Please log in to continue</h2>
+
+    return (props.isLoggedIn ? welcomeMessage : loginPrompt )
+}
+export default UserGreeting
