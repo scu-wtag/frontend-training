fd6a6fe1cf68d7a1771a592acba08c9a0cc7d993
scu-wtag <sophia.cuarte@welldev.io>
2025-10-08 11:23:18 +0200

    styled components

--- React/TeamMemberCard/src/App.jsx
+++ React/TeamMemberCard/src/App.jsx
@@ -1,25 +1,40 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import styled from "styled-components";
 import { TeamMember } from "./TeamMember";
+
+const TeamContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin-top: 200px;
+`;
+
+const TeamCards = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  gap: 20px;
+  margin-top: 2rem;
+`;
 
 function App() {
   const team = [
-    { name: "Amy", age: 30, pet: "Rabbit called Buns" },
-    { name: "Thomas", age: 49, pet: "Budgi names Tim" },
-    { name: "Joanna", age: 25, pet: "Golden Retriever called Buddy" },
-    { name: "Jake", age: 19, pet: "Axolotl named Axy" },
+    { name: "Amy", age: 30, pet: " Rabbit called Buns" },
+    { name: "Thomas", age: 49, pet: " Budgie named Tim" },
+    { name: "Joanna", age: 25, pet: " Golden Retriever called Buddy" },
+    { name: "Jake", age: 19, pet: "n Axolotl named Axy" },
   ];
+
   return (
-    <div className="team-container">
+    <TeamContainer>
       <h1>Our Team</h1>
-      <div className="team-cards">
-        {team.map((u) => (
-          <TeamMember name={u.name} age={u.age} pet={u.pet} />
+      <TeamCards>
+        {team.map((u, i) => (
+          <TeamMember key={i} name={u.name} age={u.age} pet={u.pet} />
         ))}
-      </div>
-    </div>
+      </TeamCards>
+    </TeamContainer>
   );
 }
 
--- React/TeamMemberCard/src/TeamMember.jsx
+++ React/TeamMemberCard/src/TeamMember.jsx
@@ -1,11 +1,25 @@
 import "./App.css";
+import styled from "styled-components";
+
+const TeamMemberContainer = styled.div `
+  max-width: 180px;
+  background-color: #f9f9f9;
+  padding: 2rem;
+  border-radius: 12px;
+  box-shadow: 0 4px 12px rgba(197, 15, 15, 0.48);
+  text-align: center;
+  color: black;
+`
 
 export function TeamMember(props) {
   return (
-    <div className="team-member-container">
-      <p>Hello my name is {props.name}</p>
-      <p>I am {props.age} years old</p>
-      <p>I have a {props.pet}</p>
-    </div>
+    <TeamMemberContainer>
+      <div>
+        <p>Hello my name is {props.name}</p>
+        <p>I am {props.age} years old</p>
+        <p>I have a{props.pet}</p>
+      </div>
+    </TeamMemberContainer>
+
   );
 }
