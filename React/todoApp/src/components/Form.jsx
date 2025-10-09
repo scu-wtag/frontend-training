e9811884822307ba9ef813f2152660cfe9dfa126
scu-wtag <sophia.cuarte@welldev.io>
2025-10-09 08:55:19 +0200

    todoApp done

---
+++ React/todoApp/src/components/Form.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import styles from "./Form.module.css";
+
+export default function Form({ setTodos }) {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const value = e.target.todo.value.trim();
+    if (!value) return;
+
+    setTodos(prev => [
+      ...prev,
+      { id: crypto.randomUUID(), title: value, is_completed: false }
+    ]);
+
+    e.target.reset();
+  };
+
+  return (
+    <form className={styles.form} onSubmit={handleSubmit}>
+      <label className={styles.label} htmlFor="todo">
+        <input
+          className={styles.input}
+          name="todo"
+          id="todo"
+          placeholder="Write your next "
+        />
+      </label>
+      <button className={styles.button} type="submit">Add</button>
+    </form>
+  );
+}
